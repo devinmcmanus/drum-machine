37df6b2d4816b3d232de8af553a3767570161c36
Devin McManus <devin.mcmanus@gmail.com>
2024-09-08 22:34:25 -0400

    Apply keyboard handling

--- src/App.js
+++ src/App.js
@@ -2,49 +2,75 @@ import React from 'react';
 import DrumPad from './DrumPad';
 import './App.css';
 
-function App() {
-  return (
-    <div id='drum-machine' className='container'>
-      <div className='row'>
-        <div className='col'>
-          <div id='display' />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress(event) {    
+    const key = event.key;
+    const keyUpper = key.toUpperCase();
+    const keysToHandle = 'QWEASDZXC';
+    
+    if (keysToHandle.includes(keyUpper)) {
+      document.getElementById(keyUpper).click();
+    }
+  }
+
+  render() {
+    return (
+      <div id='drum-machine' className='container'>
+        <div className='row'>
+          <div className='col'>
+            <div id='display' />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <DrumPad sample='Heater-1' keyBind='Q' />
+        <div className='row'>
+          <div className='col'>
+            <DrumPad sample='Heater-1' keyBind='Q' />
+          </div>
+          <div className='col'>
+            <DrumPad sample='Heater-2' keyBind='W' />
+          </div>
+          <div className='col'>
+            <DrumPad sample='Heater-3' keyBind='E' />
+          </div>
         </div>
-        <div className='col'>
-          <DrumPad sample='Heater-2' keyBind='W' />
+        <div className='row'>
+          <div className='col'>
+            <DrumPad sample='Heater-4_1' keyBind='A' />
+          </div>
+          <div className='col'>
+            <DrumPad sample='Heater-6' keyBind='S' />
+          </div>
+          <div className='col'>
+            <DrumPad sample='Dsc_Oh' keyBind='D' />
+          </div>
         </div>
-        <div className='col'>
-          <DrumPad sample='Heater-3' keyBind='E' />
+        <div className='row'>
+          <div className='col'>
+            <DrumPad sample='Kick_n_Hat' keyBind='Z' />
+          </div>
+          <div className='col'>
+            <DrumPad sample='RP4_KICK_1' keyBind='X' />
+          </div>
+          <div className='col'>
+            <DrumPad sample='Cev_H2' keyBind='C' />
+          </div>
         </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <DrumPad sample='Heater-4_1' keyBind='A' />
-        </div>
-        <div className='col'>
-          <DrumPad sample='Heater-6' keyBind='S' />
-        </div>
-        <div className='col'>
-          <DrumPad sample='Dsc_Oh' keyBind='D' />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <DrumPad sample='Kick_n_Hat' keyBind='Z' />
-        </div>
-        <div className='col'>
-          <DrumPad sample='RP4_KICK_1' keyBind='X' />
-        </div>
-        <div className='col'>
-          <DrumPad sample='Cev_H2' keyBind='C' />
-        </div>
-      </div>
-    </div>          
-  );
+      </div>          
+    );
+  }
 }
 
 export default App;
