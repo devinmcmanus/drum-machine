f5b86e02860ad73555d9b2fda0ddbaeba1ff1b4b
Devin McManus <devin.mcmanus@gmail.com>
2024-09-08 20:12:55 -0400

    Include FCC tests and Bootstrap

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('it should render an outer container with id="drum-machine" that contains all elements', () => {
+  // Render the app
+  const { container } = render(<App />);
+
+  // Get the container by its id
+  const drumMachineContainer = container.querySelector('#drum-machine');
+
+  // Check if the container is in the document
+  expect(drumMachineContainer).toBeInTheDocument();
+});
+
+test('it should have an element with id="display" within id="drum-machine"', () => {
+  const { container } = render(<App />);
+
+  const displayContainer = container.querySelector('#drum-machine #display');
+
+  expect(displayContainer).toBeInTheDocument();
 });
